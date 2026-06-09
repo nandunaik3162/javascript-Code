c49852404b660bbb659d554d4d37d43564642442
nandunaik3162 <lakavathramulu2005@gmail.com>
2026-06-09 17:27:39 +0530

    Add files via upload

---
+++ lec4_cac.js
@@ -0,0 +1,20 @@
+// constant and variables
+
+const account_no = 535367373
+let account_email="nandu@gmail.com"
+var account_password="121342" // should not use 
+account_branch="Hyderabad"  //should not use
+
+let account_type
+
+// account_no = 43536363 // not allowed as we are using const.
+account_email="nithin@gmail.com"
+account_password="42543536"
+account_branch="Suryapet"
+
+
+console.log(account_no)
+
+
+
+console.table([account_no,account_email,account_password,account_branch,account_type])
